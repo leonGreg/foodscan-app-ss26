8216f57a4899ce66eef627734006612be925d67e
Elsayed,Islam <islam.elsayed@study.thws.de>
2026-05-13 21:17:34 +0200

    Add recent scans pagination state

--- lib/features/home/presentation/bloc/home_state.dart
+++ lib/features/home/presentation/bloc/home_state.dart
@@ -15,13 +15,55 @@ class HomeLoading extends HomeState {
   const HomeLoading();
 }
 
+// class HomeLoaded extends HomeState {
+//   final List<ScanRecord> recentScans;
+
+//   const HomeLoaded({required this.recentScans});
+
+//   @override
+//   List<Object?> get props => [recentScans];
+// }
+
 class HomeLoaded extends HomeState {
   final List<ScanRecord> recentScans;
+  
+  //query is needed to let the Bloc knows when the user is searching. while searching we stop infinite scrolling
+  final String query;
 
-  const HomeLoaded({required this.recentScans});
+  final bool hasMoreRecentScans;
+  final bool isLoadingMoreRecentScans;
+
+  const HomeLoaded({
+    required this.recentScans,
+    this.query = '',
+    this.hasMoreRecentScans = false,
+    this.isLoadingMoreRecentScans = false,
+  });
+
+  bool get isSearchMode => query.trim().isNotEmpty;
+
+  HomeLoaded copyWith({
+    List<ScanRecord>? recentScans,
+    String? query,
+    bool? hasMoreRecentScans,
+    bool? isLoadingMoreRecentScans,
+  }) {
+    return HomeLoaded(
+      recentScans: recentScans ?? this.recentScans,
+      query: query ?? this.query,
+      hasMoreRecentScans: hasMoreRecentScans ?? this.hasMoreRecentScans,
+      isLoadingMoreRecentScans:
+          isLoadingMoreRecentScans ?? this.isLoadingMoreRecentScans,
+    );
+  }
 
   @override
-  List<Object?> get props => [recentScans];
+  List<Object?> get props => [
+        recentScans,
+        query,
+        hasMoreRecentScans,
+        isLoadingMoreRecentScans,
+      ];
 }
 
 class HomeError extends HomeState {
