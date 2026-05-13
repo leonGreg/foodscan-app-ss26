300f0bae6d753b97419d256488f7b15f63a135a0
Elsayed,Islam <islam.elsayed@study.thws.de>
2026-05-13 23:13:00 +0200

    Add search pagination state

--- lib/features/home/presentation/bloc/home_state.dart
+++ lib/features/home/presentation/bloc/home_state.dart
@@ -15,45 +15,50 @@ class HomeLoading extends HomeState {
   const HomeLoading();
 }
 
-// class HomeLoaded extends HomeState {
-//   final List<ScanRecord> recentScans;
-
-//   const HomeLoaded({required this.recentScans});
-
-//   @override
-//   List<Object?> get props => [recentScans];
-// }
-
 class HomeLoaded extends HomeState {
   final List<ScanRecord> recentScans;
   
   //query is needed to let the Bloc knows when the user is searching. while searching we stop infinite scrolling
   final String query;
-
+  
+  //recentScan pagination
   final bool hasMoreRecentScans;
   final bool isLoadingMoreRecentScans;
+
+  //search pagination
+  final bool hasMoreSearchResults;
+  final bool isLoadingMoreSearchResults;
 
   const HomeLoaded({
     required this.recentScans,
     this.query = '',
     this.hasMoreRecentScans = false,
     this.isLoadingMoreRecentScans = false,
+    this.hasMoreSearchResults = false,
+    this.isLoadingMoreSearchResults = false,
   });
 
   bool get isSearchMode => query.trim().isNotEmpty;
+
+  bool get isLoadingMoreVisible => isSearchMode ? isLoadingMoreSearchResults : isLoadingMoreRecentScans;
+
+  bool get hasMoreVisible => isSearchMode ? hasMoreSearchResults : hasMoreRecentScans;
 
   HomeLoaded copyWith({
     List<ScanRecord>? recentScans,
     String? query,
     bool? hasMoreRecentScans,
     bool? isLoadingMoreRecentScans,
+    bool? hasMoreSearchResults,
+    bool? isLoadingMoreSearchResults,
   }) {
     return HomeLoaded(
       recentScans: recentScans ?? this.recentScans,
       query: query ?? this.query,
       hasMoreRecentScans: hasMoreRecentScans ?? this.hasMoreRecentScans,
-      isLoadingMoreRecentScans:
-          isLoadingMoreRecentScans ?? this.isLoadingMoreRecentScans,
+      isLoadingMoreRecentScans: isLoadingMoreRecentScans ?? this.isLoadingMoreRecentScans,
+      hasMoreSearchResults: hasMoreSearchResults ?? this.hasMoreSearchResults,
+      isLoadingMoreSearchResults: isLoadingMoreSearchResults ?? this.isLoadingMoreSearchResults,
     );
   }
 
@@ -63,6 +68,8 @@ class HomeLoaded extends HomeState {
         query,
         hasMoreRecentScans,
         isLoadingMoreRecentScans,
+        hasMoreSearchResults,
+        isLoadingMoreSearchResults,
       ];
 }
 
