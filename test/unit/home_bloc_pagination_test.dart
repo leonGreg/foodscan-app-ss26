1b1b12b822cb26f5d5cba2657de4b36a471832b9
Elsayed,Islam <islam.elsayed@study.thws.de>
2026-05-21 15:59:20 +0200

    test: add fake scan repository for pagination tests

--- test/unit/home_bloc_pagination_test.dart
+++ test/unit/home_bloc_pagination_test.dart
@@ -1,6 +1,9 @@
 import 'package:food_scan/core/models/product_model.dart';
 import 'package:food_scan/features/home/data/models/scan_record.dart';
 import 'package:food_scan/features/home/data/repositories/scan_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:food_scan/features/home/presentation/bloc/home_bloc.dart';
 
 ScanRecord scan(String barcode, String name) {
   final product = Product(
@@ -22,3 +25,69 @@ ScanPage page(
     hasMore: hasMore,
   );
 }
+
+class FakeScanRepository implements ScanRepository {
+  FakeScanRepository({
+    List<ScanPage>? recentPages,
+    List<ScanPage>? searchPages,
+  })  : recentPages = recentPages ?? [],
+        searchPages = searchPages ?? [];
+
+  final List<ScanPage> recentPages;
+  final List<ScanPage> searchPages;
+
+  int getScansPageCallCount = 0;
+  int searchScansPageCallCount = 0;
+  int saveScanCallCount = 0;
+
+  final List<String> searchedQueries = [];
+
+  Future<ScanPage> getScansPage(
+    String uid, {
+    int limit = 20,
+    DocumentSnapshot<Map<String, dynamic>>? startAfterDocument,
+  }) async {
+    getScansPageCallCount++;
+
+    if (recentPages.isEmpty) {
+      return page([], hasMore: false);
+    }
+
+    return recentPages.removeAt(0);
+  }
+
+  Future<ScanPage> searchScansPage(
+    String uid, {
+    required String queryText,
+    int limit = 20,
+    DocumentSnapshot<Map<String, dynamic>>? startAfterDocument,
+  }) async {
+    searchScansPageCallCount++;
+    searchedQueries.add(queryText);
+
+    if (searchPages.isEmpty) {
+      return page([], hasMore: false);
+    }
+
+    return searchPages.removeAt(0);
+  }
+
+  Future<void> saveScan(String uid, ScanRecord scan) async {
+    saveScanCallCount++;
+  }
+
+  Future<List<ScanRecord>> getScans(String uid) async {
+    // Delegate to getScansPage to mimic real repository behavior
+    final page = await getScansPage(uid, limit: 20);
+    return page.scans;
+  }
+}
+
+HomeBloc createBloc(FakeScanRepository repository) {
+  return HomeBloc(
+    scanRepository: repository,
+    currentUidProvider: () => 'test-user-id',
+    authStateChanges: const Stream<User?>.empty(),
+  );
+}
+
